e1b74ffbe942ea777553af1bb19e33ef5d907b2d
Peter Windas <peterwindas@wireless-10-105-149-27.umd.edu>
2020-12-01 15:22:50 -0500

    testLogin commit 2

--- TestLogin/app/src/main/java/com/example/studentexpensetracker/MainActivity.kt
+++ TestLogin/app/src/main/java/com/example/studentexpensetracker/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.studentexpensetracker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var registerBtn: Button
+    private lateinit var loginBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initializeViews()
+
+        registerBtn.setOnClickListener {
+            startActivity(Intent(this@MainActivity, RegistrationActivity::class.java))
+        }
+        loginBtn.setOnClickListener {
+            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+        }
+    }
+
+    private fun initializeViews() {
+        registerBtn = findViewById(R.id.register)
+        loginBtn = findViewById(R.id.login)
     }
 }
